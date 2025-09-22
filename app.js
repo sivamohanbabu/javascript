caba5ffcaea23098dfd8338a53d8698483410b6d
sivamohanbabu <sivamohanbabu@gmail.com>
2025-09-22 10:27:51 +0530

    variable

---
+++ app.js
@@ -0,0 +1,14 @@
+//Types of variables
+//var, let, const
+//var - global variable
+//let,const - local variable
+
+var a = 67;
+var a = 90;
+let b = 68;
+
+const c = 78;
+
+console.log(`value b is ${b}`);
+console.log(`value c is ${c}`);
+console.log(a);
