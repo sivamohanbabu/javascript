eea23c8a3804d08604f52c07c2ce41875c429b8d
sivamohanbabu <sivamohanbabu@gmail.com>
2025-09-22 16:09:19 +0530

    functions

---
+++ predefined.js
@@ -0,0 +1,16 @@
+var a = Number(prompt("Enter value a  "));
+var b = Number(prompt("Enter value b  "));
+
+document.write(`Entered values and b is ${a} ,${b}`);
+function add() {
+  alert(a + b);
+}
+function sub() {
+  alert(a - b);
+}
+function mul() {
+  alert(a * b);
+}
+function div() {
+  alert(a / b);
+}
